09ea733a05a984431370eb195d44aa5735da2b22
Aaron Dodd <me@aaronrdodd.com>
2025-08-13 09:08:20 +0100

    Add fnm to powershell configuration

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,7 @@
-oh-my-posh init pwsh | Invoke-Expression
+if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
+    oh-my-posh init pwsh | Invoke-Expression
+}
+
+if (Get-Command fnm -ErrorAction SilentlyContinue) {
+    fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
+}
